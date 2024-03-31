c8e6b433c579ca3feb7a80581d74a58cbc639bd6
Geovanny Bucay <ebucayle@pichincha.com>
2024-03-31 18:53:14 -0500

    pagination

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -2,6 +2,7 @@ import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { PrismaClient } from '@prisma/client';
+import { PaginationDto } from 'src/common';
 
 @Injectable()
 export class ProductsService extends PrismaClient implements OnModuleInit {
@@ -16,8 +17,21 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
     });
   }
 
-  findAll() {
-    return `This action returns all products`;
+  async findAll(paginationDto: PaginationDto) {
+    const { limit, page } = paginationDto;
+    const totalPage = await this.product.count();
+    const lastPage = Math.ceil(totalPage / limit);
+    return {
+      data: await this.product.findMany({
+        take: limit,
+        skip: limit * (page - 1),
+      }),
+      meta: {
+        totalPage,
+        currentPage: page,
+        lastPage,
+      },
+    };
   }
 
   findOne(id: number) {
